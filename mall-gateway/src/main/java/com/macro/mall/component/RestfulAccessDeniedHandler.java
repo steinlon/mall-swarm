3cd97964814220580f0df7acdcec87506c034eaf
fulong.shi <fulong.shi@ysura.com>
2021-07-28 08:33:23 +0200

    update formate and learn about infrastructure of project

--- mall-gateway/src/main/java/com/macro/mall/component/RestfulAccessDeniedHandler.java
+++ mall-gateway/src/main/java/com/macro/mall/component/RestfulAccessDeniedHandler.java
@@ -22,15 +22,16 @@ import java.nio.charset.Charset;
  */
 @Component
 public class RestfulAccessDeniedHandler implements ServerAccessDeniedHandler {
+
     @Override
     public Mono<Void> handle(ServerWebExchange exchange, AccessDeniedException denied) {
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(HttpStatus.OK);
         response.getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        response.getHeaders().set("Access-Control-Allow-Origin","*");
-        response.getHeaders().set("Cache-Control","no-cache");
-        String body= JSONUtil.toJsonStr(CommonResult.forbidden(denied.getMessage()));
-        DataBuffer buffer =  response.bufferFactory().wrap(body.getBytes(Charset.forName("UTF-8")));
+        response.getHeaders().set("Access-Control-Allow-Origin", "*");
+        response.getHeaders().set("Cache-Control", "no-cache");
+        String body = JSONUtil.toJsonStr(CommonResult.forbidden(denied.getMessage()));
+        DataBuffer buffer = response.bufferFactory().wrap(body.getBytes(Charset.forName("UTF-8")));
         return response.writeWith(Mono.just(buffer));
     }
 }
--- mall-gateway/src/main/java/com/macro/mall/handler/SwaggerHandler.java
+++ mall-gateway/src/main/java/com/macro/mall/handler/SwaggerHandler.java
@@ -6,7 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-import springfox.documentation.swagger.web.*;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 
 import java.util.Optional;
 
