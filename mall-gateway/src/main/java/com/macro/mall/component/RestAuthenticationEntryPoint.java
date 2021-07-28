168bb279d4f46c547006575f96b19b607df3d13f
fulong.shi <fulong.shi@ysura.com>
2021-07-28 08:33:23 +0200

    update formate and learn about infrastructure of project

--- mall-gateway/src/main/java/com/macro/mall/component/RestAuthenticationEntryPoint.java
+++ mall-gateway/src/main/java/com/macro/mall/component/RestAuthenticationEntryPoint.java
@@ -21,15 +21,16 @@ import java.nio.charset.Charset;
  */
 @Component
 public class RestAuthenticationEntryPoint implements ServerAuthenticationEntryPoint {
+
     @Override
     public Mono<Void> commence(ServerWebExchange exchange, AuthenticationException e) {
         ServerHttpResponse response = exchange.getResponse();
         response.setStatusCode(HttpStatus.OK);
         response.getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        response.getHeaders().set("Access-Control-Allow-Origin","*");
-        response.getHeaders().set("Cache-Control","no-cache");
-        String body= JSONUtil.toJsonStr(CommonResult.unauthorized(e.getMessage()));
-        DataBuffer buffer =  response.bufferFactory().wrap(body.getBytes(Charset.forName("UTF-8")));
+        response.getHeaders().set("Access-Control-Allow-Origin", "*");
+        response.getHeaders().set("Cache-Control", "no-cache");
+        String body = JSONUtil.toJsonStr(CommonResult.unauthorized(e.getMessage()));
+        DataBuffer buffer = response.bufferFactory().wrap(body.getBytes(Charset.forName("UTF-8")));
         return response.writeWith(Mono.just(buffer));
     }
 }
