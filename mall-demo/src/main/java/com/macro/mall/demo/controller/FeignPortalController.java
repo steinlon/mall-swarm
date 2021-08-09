1e3899a5555dd0ddc80567df145dac086ef79644
fulong.shi <fulong.shi@ysura.com>
2021-08-09 19:01:53 +0200

    Update system configuration
    Add endpoint to retrieve nacos service in spring cloud
    Format code in demo module

--- mall-demo/src/main/java/com/macro/mall/demo/controller/FeignPortalController.java
+++ mall-demo/src/main/java/com/macro/mall/demo/controller/FeignPortalController.java
@@ -3,24 +3,27 @@ package com.macro.mall.demo.controller;
 import com.macro.mall.common.api.CommonResult;
 import com.macro.mall.demo.service.FeignPortalService;
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Feign调用mall-portal接口示例
- * Created by macro on 2019/10/18.
  */
 @Api(tags = "FeignPortalController", description = "Feign调用mall-portal接口示例")
 @RestController
 @RequestMapping("/feign/portal")
+@AllArgsConstructor
 public class FeignPortalController {
 
-    @Autowired
-    private FeignPortalService portalService;
+    private final FeignPortalService portalService;
 
     @PostMapping("/login")
-    public CommonResult<?> login(@RequestParam String username, @RequestParam String password) {
-        return portalService.login(username,password);
+    public CommonResult<?> login(@RequestParam final String username, @RequestParam final String password) {
+        return portalService.login(username, password);
     }
 
     @GetMapping("/cartList")
