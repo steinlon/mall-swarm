37ae0563bbf94958af82abc365a68bab823fb14e
fulong.shi <fulong.shi@ysura.com>
2021-10-08 19:44:55 +0200

    Format code in portal module

--- mall-portal/src/main/java/com/macro/mall/portal/component/CancelOrderReceiver.java
+++ mall-portal/src/main/java/com/macro/mall/portal/component/CancelOrderReceiver.java
@@ -1,26 +1,26 @@
 package com.macro.mall.portal.component;
 
 import com.macro.mall.portal.service.OmsPortalOrderService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
  * 取消订单消息的处理者
- * Created by macro on 2018/9/14.
  */
+@AllArgsConstructor
 @Component
 @RabbitListener(queues = "mall.order.cancel")
+@Slf4j
 public class CancelOrderReceiver {
-    private static Logger LOGGER =LoggerFactory.getLogger(CancelOrderReceiver.class);
-    @Autowired
-    private OmsPortalOrderService portalOrderService;
+
+    private final OmsPortalOrderService portalOrderService;
+
     @RabbitHandler
-    public void handle(Long orderId){
+    public void handle(final Long orderId) {
         portalOrderService.cancelOrder(orderId);
-        LOGGER.info("process orderId:{}",orderId);
+        log.info("process orderId:{}", orderId);
     }
 }
--- mall-portal/src/main/java/com/macro/mall/portal/dao/PortalProductDao.java
+++ mall-portal/src/main/java/com/macro/mall/portal/dao/PortalProductDao.java
@@ -9,10 +9,12 @@ import java.util.List;
 
 /**
  * 前台系统自定义商品Dao
- * Created by macro on 2018/8/2.
  */
 public interface PortalProductDao {
+
     CartProduct getCartProduct(@Param("id") Long id);
+
     List<PromotionProduct> getPromotionProductList(@Param("ids") List<Long> ids);
-    List<SmsCoupon> getAvailableCouponList(@Param("productId") Long productId,@Param("productCategoryId")Long productCategoryId);
+
+    List<SmsCoupon> getAvailableCouponList(@Param("productId") Long productId, @Param("productCategoryId") Long productCategoryId);
 }
