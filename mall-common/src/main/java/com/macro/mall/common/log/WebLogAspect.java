6627f465f8acc7fabe1065f8f73a5212728812d3
fulong.shi <fulong.shi@ysura.com>
2021-11-13 16:56:55 +0100

    More study about elastic search, kibana and logstash

--- mall-common/src/main/java/com/macro/mall/common/log/WebLogAspect.java
+++ mall-common/src/main/java/com/macro/mall/common/log/WebLogAspect.java
@@ -16,8 +16,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
